9e9970b5ccf55d335d21bb3dc7a10a52e2c67e70
subhamdev245 <dev.subham05@gmail.com>
2024-03-28 18:42:35 +0530

    Sign In and SIgn Up Added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Sign from "./Sign.jsx"
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <img src="https://wallpapers.com/images/high/netflix-background-gs7hjuwvv2g0e9fj.webp" alt=""  className='w-full'/>
+       <Sign />
+    </>
+  )
+}
+
+export default App
